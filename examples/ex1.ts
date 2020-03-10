e6644580a25a873d16c6e2db8d6b9d929781367a
jacobbubu <rong.shen@gmail.com>
2020-03-10 17:22:04 +0800

    docs: add more use cases to the example:ex1

--- examples/ex1.ts
+++ examples/ex1.ts
@@ -4,5 +4,8 @@ console.log(between('A', 'B'))
 // AV
 
 const binaryBetween = inject('01').between
-console.log(binaryBetween('0', '0', true))
+console.log(binaryBetween('0', '1'))
 // 01
+
+console.log(binaryBetween('0', '0'))
+// Error: Impossible to generate a string that lexicographically sorts between '0' and '0'
